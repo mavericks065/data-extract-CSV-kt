be8e67c9f102c2fb0a448815ee71af9cab91b73f
nicolas <nicolas065@gmail.com>
2021-03-20 16:15:58 +1100

    add gender and age in the model and make tests pass

--- src/main/kotlin/au/com/nig/pks/Main.kt
+++ src/main/kotlin/au/com/nig/pks/Main.kt
@@ -1,8 +1,9 @@
 package au.com.nig.pks
 
 fun main(args: Array<String>) {
-    IO.readFile(args[0])
+    val patients = IO.readFile(args[0])
         .groupBy { it.get("PatientId") }
         .map { (key, records) -> Patient.transformToPatient(key, records) }
+
 
 }
--- src/main/kotlin/au/com/nig/pks/Patient.kt
+++ src/main/kotlin/au/com/nig/pks/Patient.kt
@@ -3,18 +3,23 @@ package au.com.nig.pks
 import org.apache.commons.csv.CSVRecord
 import java.time.LocalDate
 
-data class Patient(val id: Long, val patientDetails: List<PatientDetails>) {
+data class Patient(val id: Long, val age: Long?, val gender: String?, val patientDetails: List<PatientDetails>) {
 
     companion object {
         fun transformToPatient(key: String, records: List<CSVRecord>): Patient {
-            val patientDetails = records.map {
+            val age = records.find { it[2].equals("Age") }?.let { it.get(3)?.toLong() }
+            val gender = records.find { it[2].equals("Gender") }?.get(3)
+
+            val patientDetails = records.filter { !it[2].equals("Age") && !it[2].equals("Gender") }.map {
                 PatientDetails(
                     detail = it[2],
                     value = it[3],
                     date = LocalDate.parse(it[0])
                 )
             }
-            return Patient(key.toLong(), patientDetails)
+            return Patient(
+                id = key.toLong(), age = age, gender = gender, patientDetails = patientDetails
+            )
         }
     }
 }
