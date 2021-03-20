e9be336c114410c4d99e6003b30c346bcb963bb2
nicolas <nicolas065@gmail.com>
2021-03-20 16:38:20 +1100

    add compute avg age per gender

--- src/main/kotlin/au/com/nig/pks/ReportingEngine.kt
+++ src/main/kotlin/au/com/nig/pks/ReportingEngine.kt
@@ -4,5 +4,10 @@ class ReportingEngine {
     companion object {
         fun computePatientsOnGender(patients: List<Patient>, gender: String): Int =
             patients.filter { it.gender.equals(gender) }.size
+
+        fun computeAverageAgeOfGender(patients: List<Patient>, gender: String): Double =
+            patients.filter { it.gender.equals(gender) }
+                .mapNotNull { it.age }
+                .average()
     }
 }
--- src/test/kotlin/au/com/nig/pks/ReportingEngineTest.kt
+++ src/test/kotlin/au/com/nig/pks/ReportingEngineTest.kt
@@ -44,4 +44,43 @@ object ReportingEngineTest: Spek({
             assertEquals(1, result)
         }
     }
+
+    describe("computeAverageAgeOfGender") {
+        it("should return the avg age of males") {
+            // Given
+            val expectedDetails = listOf(
+                PatientDetails("Blood pressure", "160", LocalDate.of(2019, 12, 9)),
+                PatientDetails("Glucose", "11.1", LocalDate.of(2019, 12, 9)),
+                PatientDetails("Diabetes", "TRUE", LocalDate.of(2019, 12, 9)),
+                PatientDetails("WCC", "120", LocalDate.of(2019, 12, 9))
+            )
+            val patient0 = Patient(1L, 23L, "M", expectedDetails)
+            val patient1 = Patient(2L, 30L, "F", expectedDetails)
+            val patient2 = Patient(3L, 30L, "M", expectedDetails)
+
+            // When
+            val result = ReportingEngine.computeAverageAgeOfGender(listOf(patient0, patient1, patient2), "M")
+
+            // Then
+            assertEquals(26.5, result)
+        }
+        it("should return the avg age of females") {
+            // Given
+            val expectedDetails = listOf(
+                PatientDetails("Blood pressure", "160", LocalDate.of(2019, 12, 9)),
+                PatientDetails("Glucose", "11.1", LocalDate.of(2019, 12, 9)),
+                PatientDetails("Diabetes", "TRUE", LocalDate.of(2019, 12, 9)),
+                PatientDetails("WCC", "120", LocalDate.of(2019, 12, 9))
+            )
+            val patient0 = Patient(1L, 23L, "F", expectedDetails)
+            val patient1 = Patient(2L, 30L, "F", expectedDetails)
+            val patient2 = Patient(3L, 30L, "M", expectedDetails)
+
+            // When
+            val result = ReportingEngine.computeAverageAgeOfGender(listOf(patient0, patient1, patient2), "F")
+
+            // Then
+            assertEquals(26.5, result)
+        }
+    }
 })
