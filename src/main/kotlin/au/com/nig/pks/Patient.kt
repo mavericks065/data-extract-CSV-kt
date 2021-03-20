80b65d62e7c8586cd540463fc48dafd689c899a3
nicolas <nicolas065@gmail.com>
2021-03-20 16:05:24 +1100

    make test pass

--- src/main/kotlin/au/com/nig/pks/Patient.kt
+++ src/main/kotlin/au/com/nig/pks/Patient.kt
@@ -6,7 +6,16 @@ import java.time.LocalDate
 data class Patient(val id: Long, val patientDetails: List<PatientDetails>) {
 
     companion object {
-        fun transformToPatient(key: String, records: List<CSVRecord>): Patient = TODO()
+        fun transformToPatient(key: String, records: List<CSVRecord>): Patient {
+            val patientDetails = records.map {
+                PatientDetails(
+                    detail = it[2],
+                    value = it[3],
+                    date = LocalDate.parse(it[0])
+                )
+            }
+            return Patient(key.toLong(), patientDetails)
+        }
     }
 }
 
--- src/test/kotlin/au/com/nig/pks/PatientTest.kt
+++ src/test/kotlin/au/com/nig/pks/PatientTest.kt
@@ -11,26 +11,27 @@ object PatientTest : Spek({
     describe("When receiving an id and a list of CSV records") {
         it("should transform it into a patient with all its details") {
             // Given
-            val key = "1"
-            val currentRelativePath: Path = Paths.get("")
-            val s = currentRelativePath.toAbsolutePath().toString()
-            val filePath = "$s/src/test/resources/short_sample.csv"
-            val records = IO.readFile(filePath)
-
             val expectedDetails = listOf(
                 PatientDetails("Age", "23", LocalDate.of(2019, 12, 9)),
                 PatientDetails("Blood pressure", "160", LocalDate.of(2019, 12, 9)),
                 PatientDetails("Gender", "F", LocalDate.of(2019, 12, 9)),
                 PatientDetails("Glucose", "11.1", LocalDate.of(2019, 12, 9)),
                 PatientDetails("Diabetes", "TRUE", LocalDate.of(2019, 12, 9)),
-                PatientDetails("WCC", "WCC", LocalDate.of(2019, 12, 9))
+                PatientDetails("WCC", "120", LocalDate.of(2019, 12, 9))
             )
+            val expectedPatient = Patient(1L, expectedDetails)
+
+            val key = "1"
+            val currentRelativePath: Path = Paths.get("")
+            val s = currentRelativePath.toAbsolutePath().toString()
+            val filePath = "$s/src/test/resources/short_sample.csv"
+            val records = IO.readFile(filePath)
 
             // When
             val result = Patient.transformToPatient(key, records)
 
             // Then
-            assertEquals(Patient(1L, expectedDetails), result)
+            assertEquals(expectedPatient, result)
         }
     }
 })
