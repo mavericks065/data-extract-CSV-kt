4cabaaf9f5a1a6cfb5d0b46339beb2b6ee3feb88
nicolas <nicolas065@gmail.com>
2021-03-20 15:27:15 +1100

    make test pass

--- src/main/kotlin/au/com/nig/pks/IO.kt
+++ src/main/kotlin/au/com/nig/pks/IO.kt
@@ -1,7 +1,17 @@
 package au.com.nig.pks
 
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
 import org.apache.commons.csv.CSVRecord
+import java.nio.file.Paths
 
 object IO {
-    fun readFile(filePath: String): List<CSVRecord> = TODO ()
+    fun readFile(filePath: String): List<CSVRecord> {
+        val bufReader = Paths.get(filePath).toFile().bufferedReader()
+        val parser = CSVParser(
+            bufReader,
+            CSVFormat.DEFAULT.withFirstRecordAsHeader().withTrim()
+        )
+        return parser.records
+    }
 }
