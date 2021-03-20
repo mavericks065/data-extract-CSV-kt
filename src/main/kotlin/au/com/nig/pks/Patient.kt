3b54027bb14d8553e88d00f92ae558771a822a7e
nicolas <nicolas065@gmail.com>
2021-03-20 15:42:40 +1100

    add next step

--- src/main/kotlin/au/com/nig/pks/Patient.kt
+++ src/main/kotlin/au/com/nig/pks/Patient.kt
@@ -3,11 +3,11 @@ package au.com.nig.pks
 import org.apache.commons.csv.CSVRecord
 import java.time.LocalDate
 
-data class Patient(val id: Long, val testResults: List<TestResult>) {
+data class Patient(val id: Long, val patientDetails: List<PatientDetails>) {
 
     companion object {
         fun transformToPatient(key: String, records: List<CSVRecord>): Patient = TODO()
     }
 }
 
-data class TestResult(val test: String, val value: String, val date: LocalDate)
+data class PatientDetails(val test: String, val value: String, val date: LocalDate)
